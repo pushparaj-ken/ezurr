7f0eeeeefab3b05000fa41cd0990c6eb23c6d6fe
pushparaj-ken <79186935+pushparaj-ken@users.noreply.github.com>
2024-07-02 10:33:21 +0530

    completed

--- routes/users.js
+++ routes/users.js
@@ -2,9 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 const UsersControllers = require('../controllers/users.controllers');
-const validationMiddleware = require('../service/validate');
-const { CreateUserDto } = require('../dto/users');
 
-router.post('/register', validationMiddleware(CreateUserDto), UsersControllers.RegisterDetails);
+router.post('/register', UsersControllers.RegisterDetails);
 
 module.exports = router;
